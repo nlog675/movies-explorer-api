2b2d016b28ec2b6349ce88d4d02707489786bb98
nlog <nlog675@gmail.com>
2022-11-25 12:21:50 +0300

    predeploy

--- controllers/users.js
+++ controllers/users.js
@@ -11,7 +11,6 @@ const UnauthorizedError = require('../utils/UnauthorizedError');
 const { NODE_ENV, JWT_SECRET } = process.env;
 
 const getUser = (req, res, next) => {
-  console.log(req);
   User.findById(req.user._id)
     .then((user) => {
       if (user) {
