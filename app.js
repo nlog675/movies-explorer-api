1025e06e5db53e1da34ff556db1af0116d5b71c8
nlog <nlog675@gmail.com>
2022-11-27 14:14:25 +0300

    move texts into constants

--- app.js
+++ app.js
@@ -23,6 +23,12 @@ app.use(cors);
 app.use(helmet());
 app.use(requestLogger);
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.use(routes);
 
 app.use(errorLogger);
