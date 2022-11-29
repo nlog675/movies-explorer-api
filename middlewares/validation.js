df6bcce591799f7039d5183a437fa42c978fa033
nlog <nlog675@gmail.com>
2022-11-29 11:56:12 +0300

    fix validation after review

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -10,15 +10,15 @@ const userValidation = celebrate({
 
 const MovieValidation = celebrate({
   body: Joi.object().keys({
-    country: Joi.string().required().min(2).max(30),
-    director: Joi.string().required().min(2).max(30),
+    country: Joi.string().required(),
+    director: Joi.string().required(),
     duration: Joi.number().required(),
     year: Joi.string().required().min(2).max(4),
-    description: Joi.string().required(2).min(2),
+    description: Joi.string().required(),
     image: Joi.string().required().pattern(URL_REGEXP),
     trailerLink: Joi.string().required().pattern(URL_REGEXP),
-    nameRU: Joi.string().required().min(2).max(30),
-    nameEN: Joi.string().required().min(2).max(30),
+    nameRU: Joi.string().required(),
+    nameEN: Joi.string().required(),
     thumbnail: Joi.string().required().pattern(URL_REGEXP),
     movieId: Joi.number().required(),
   }),
