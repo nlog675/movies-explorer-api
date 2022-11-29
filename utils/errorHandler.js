2767c0eb26f8be7b779485558562872e5e1137c1
nlog <nlog675@gmail.com>
2022-11-29 13:12:45 +0300

    move 500 err message to constants

--- utils/errorHandler.js
+++ utils/errorHandler.js
@@ -1,5 +1,7 @@
+const { SERVER_ERROR } = require('./constants');
+
 module.exports.errorHandler = (err, req, res, next) => {
   const { statusCode = 500, message } = err;
-  res.status(statusCode).send({ message: statusCode === 500 ? 'Ошибка сервера' : message });
+  res.status(statusCode).send({ message: statusCode === 500 ? SERVER_ERROR : message });
   next();
 };
