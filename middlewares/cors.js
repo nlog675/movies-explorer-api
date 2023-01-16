8be46f371aab0a453cfb4220e700f8b0d7d78cdb
nlog <nlog675@gmail.com>
2023-01-16 15:50:29 +0300

    fixed

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -10,12 +10,14 @@ const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 module.exports = (req, res, next) => {
   const { origin } = req.headers;
   const { method } = req;
+  console.log('1', req.headers);
 
   const requestHeaders = req.headers['access-control-request-headers'];
 
   res.header('Access-Control-Allow-Credentials', true);
 
   if (allowedCors.includes(origin)) {
+    console.log('2', origin);
     res.header('Access-Control-Allow-Origin', origin);
   }
 
